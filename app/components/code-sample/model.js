8daa1bc34e154cd8fa622fffdb02548561b878d4
jean rodriguez <jcrodriguez@intellisys.com.do>
2019-02-20 15:59:44 -0500

    working code example

--- app/components/code-sample/model.js
+++ app/components/code-sample/model.js
@@ -1,22 +1,13 @@
 'use strict';
 
 module.exports.save = (uri, data) => {
-    console.log('\n\n\nFROM SAVE');
-    console.log('This is the uri ', uri);
-    console.log('this is the data ', JSON.stringify(data, null, 4));
-
+    debugger
     var Prism = require('prismjs');
   
+    data.code = data.code.replace(/\t/g, '  ');
+    
     // Returns a highlighted HTML string
     data.html = Prism.highlight(data.code, Prism.languages[data.language], data.language); 
 
     return data;
-}
-
-module.exports.render = (uri, data) => {
-    console.log('\n\n\nFROM RENDER');
-    console.log('This is the uri ', uri);
-    console.log('this is the data ', JSON.stringify(data, null, 4));
-
-    return Promise.resolve(data);
 }
