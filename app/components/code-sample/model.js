91ddc1fa2014d7e95b1cb14a274e17668e9ee1ad
jean rodriguez <jcrodriguez@intellisys.com.do>
2019-02-21 15:34:33 -0500

    change code description

--- app/components/code-sample/model.js
+++ app/components/code-sample/model.js
@@ -4,7 +4,7 @@ const Prism = require('prismjs');
 require('prismjs/components/prism-yaml');
 
 module.exports.save = (uri, data) => {
-  // Adds manual spaces, klin codemirror doesn't recognizes tab spaces
+  // Adds manual spaces, Kiln codemirror doesn't recognizes tab spaces
   data.code = data.code.replace(/\t/g, '  ');
 
   // Returns a highlighted HTML string
