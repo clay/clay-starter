4686eafaafaa1e71b4c48375c7786e082adca90d
jean rodriguez <jcrodriguez@intellisys.com.do>
2019-02-21 15:10:28 -0500

    code format

--- app/components/code-sample/model.js
+++ app/components/code-sample/model.js
@@ -1,13 +1,14 @@
 'use strict';
 const Prism = require('prismjs');
-require("prismjs/components/prism-yaml");
+
+require('prismjs/components/prism-yaml');
 
 module.exports.save = (uri, data) => {
-    //added manual spaces, klin codemirror don't recognize tab spaces
-    data.code = data.code.replace(/\t/g, '  ');
-    
-    // Returns a highlighted HTML string
-    data.html = Prism.highlight(data.code, Prism.languages[data.language], data.language); 
+  // added manual spaces, klin codemirror don't recognize tab spaces
+  data.code = data.code.replace(/\t/g, '  ');
 
-    return data;
-}
+  // Returns a highlighted HTML string
+  data.html = Prism.highlight(data.code, Prism.languages[data.language], data.language);
+
+  return data;
+};
