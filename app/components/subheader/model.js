a6f92854fbd7ef10a565b0a488a593f044505113
jean rodriguez <jcrodriguez@intellisys.com.do>
2019-02-25 13:37:37 -0500

    added css class for subheader

--- app/components/subheader/model.js
+++ app/components/subheader/model.js
@@ -4,6 +4,6 @@ const sanitize = require('../../services/universal/sanitize');
 
 module.exports.save = function (ref, data) {
   data.text = sanitize.validateTagContent(sanitize.toSmartText(data.text || ''));
-
+  data.css = data.type;
   return data;
 };
