a68b4140ae8c61c1406d7432acc809bb6c783950
Jean Carlos Rodriguez Cedeno <jc_rc1812@hotmail.com>
2019-02-27 13:36:47 -0500

    Code component subheader (#1)
    * changed subheader component to support different subheader size and hash functionality to go directly to a particular subheader

--- app/components/subheader/model.js
+++ app/components/subheader/model.js
@@ -4,6 +4,8 @@ const sanitize = require('../../services/universal/sanitize');
 
 module.exports.save = function (ref, data) {
   data.text = sanitize.validateTagContent(sanitize.toSmartText(data.text || ''));
-
+  data.css = data.type;
+  data.subheaderid = `${data.subheaderid || data.text}`.trim().replace(/\s+/g, '-');
+  data.iconcss = 'link';
   return data;
 };
