05967543b63423ec3cd26a8b10fa3a54d648506b
jean rodriguez <jcrodriguez@intellisys.com.do>
2019-02-28 09:48:45 -0500

    remove data.css and css property

--- app/components/subheader/model.js
+++ app/components/subheader/model.js
@@ -4,7 +4,6 @@ const sanitize = require('../../services/universal/sanitize');
 
 module.exports.save = function (ref, data) {
   data.text = sanitize.validateTagContent(sanitize.toSmartText(data.text || ''));
-  data.css = data.type;
   data.subheaderid = `${data.subheaderid || data.text}`.trim().replace(/\s+/g, '-');
   return data;
 };
