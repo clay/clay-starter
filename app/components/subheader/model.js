e30ffacd36ae92018776422ac3b3458230db5697
jean rodriguez <jcrodriguez@intellisys.com.do>
2019-02-28 11:01:23 -0500

    semantic html

--- app/components/subheader/model.js
+++ app/components/subheader/model.js
@@ -4,6 +4,6 @@ const sanitize = require('../../services/universal/sanitize');
 
 module.exports.save = function (ref, data) {
   data.text = sanitize.validateTagContent(sanitize.toSmartText(data.text || ''));
-  data.subheaderid = `${data.subheaderid || data.text}`.trim().replace(/\s+/g, '-');
+  data.subheaderId = `${data.subheaderId || data.text}`.trim().replace(/\s+/g, '-');
   return data;
 };
