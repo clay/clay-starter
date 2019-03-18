4a8eb38ea16eef69682113cfe08d174187e691d2
Scott Nash <scott.nash@nymag.com>
2019-03-18 13:43:49 -0400

    breaking sharepopup out into its own class

--- app/components/header/client.js
+++ app/components/header/client.js
@@ -1,60 +1,6 @@
 'use strict';
 
-const popup = require('../../services/client/popup');
-
-class sharePopUp {
-  constructor(shareLink, shareURL) {
-    this.shareLink = shareLink;
-    this.shareURL = shareURL;
-    this.shareService = this.shareLink.getAttribute('data-shareService') || null;
-    this.shareTitle = this.shareLink.getAttribute('title') || 'Clay Starter';
-
-    this.setDimensions();
-    this.addShareURL();
-    this.addClickHandler();
-  }
-
-  addShareURL() {
-    switch (this.shareService) {
-      case 'twitter':
-        this.shareLink.href = 'https://twitter.com/share?text=' + encodeURIComponent(this.shareTitle) + '&url=' + this.shareURL + '?utm_source=tw&utm_medium=s3&utm_campaign=sharebutton-t';
-        break;
-      case 'facebook':
-        this.shareLink.href = 'http://www.facebook.com/sharer/sharer.php?u=' + this.shareURL + '?utm_source=fb&utm_medium=s3&utm_campaign=sharebutton-t';
-        break;
-      default:
-    };
-  }
-
-  addClickHandler() {
-    this.shareLink.addEventListener('click', this.handleClick.bind(this));
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-
-    const dimensions = this.popupDimensions[this.shareService] || this.popupDimensions.default;
-
-    popup.openPopUp(this.shareLink.href, dimensions);
-  }
-
-  setDimensions() {
-    this.popupDimensions = {
-      default: {
-        w: 520,
-        h: 304
-      },
-      facebook: {
-        w: 520,
-        h: 304
-      },
-      twitter: {
-        w: 550,
-        h: 572
-      }
-    };
-  }
-}
+const sharePopUp = require('../../services/client/sharePopup');
 
 module.exports = () => {
   let canonicalEl = document.querySelector('link[rel="canonical"]'),
---
+++ app/services/client/sharePopup.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const popup = require('./popup');
+
+class sharePopUp {
+  constructor(shareLink, shareURL) {
+    this.shareLink = shareLink;
+    this.shareURL = shareURL;
+    this.shareService = this.shareLink.getAttribute('data-shareService') || null;
+    this.shareTitle = this.shareLink.getAttribute('title') || 'Clay Starter';
+
+    this.setDimensions();
+    this.addShareURL();
+    this.addClickHandler();
+  }
+
+  addShareURL() {
+    switch (this.shareService) {
+      case 'twitter':
+        this.shareLink.href = 'https://twitter.com/share?text=' + encodeURIComponent(this.shareTitle) + '&url=' + this.shareURL + '?utm_source=tw&utm_medium=s3&utm_campaign=sharebutton-t';
+        break;
+      case 'facebook':
+        this.shareLink.href = 'http://www.facebook.com/sharer/sharer.php?u=' + this.shareURL + '?utm_source=fb&utm_medium=s3&utm_campaign=sharebutton-t';
+        break;
+      default:
+    };
+  }
+
+  addClickHandler() {
+    this.shareLink.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    const dimensions = this.popupDimensions[this.shareService] || this.popupDimensions.default;
+
+    popup.openPopUp(this.shareLink.href, dimensions);
+  }
+
+  setDimensions() {
+    this.popupDimensions = {
+      default: {
+        w: 520,
+        h: 304
+      },
+      facebook: {
+        w: 520,
+        h: 304
+      },
+      twitter: {
+        w: 550,
+        h: 572
+      }
+    };
+  }
+}
+
+module.exports = sharePopUp;
