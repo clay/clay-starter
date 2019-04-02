e27a75422e9c6c7a7d556c4c9cf38b14fca3bcdf
Manuel Emilio Urena <manuel.ureh@hotmail.com>
2019-04-02 15:28:48 -0400

    Applies PR code review feedback

--- app/services/universal/byline.js
+++ app/services/universal/byline.js
@@ -17,16 +17,14 @@ function formatSimpleByline(opts = {}) {
     authors = _map(bylines, author => (_isObject(author) ? author.text : author));
 
   if (authors.length === 1) {
-    return '<span>' + authors[0] + '</span>';
+    return `<span>${authors[0]}</span>`;
   } else if (authors.length === 2) {
-    return (
-      '<span>' + authors[0] + '</span><span class="and"> and </span><span>' + authors[1] + '</span>'
-    );
+    return `<span>${authors[0]}</span><span class="and"> and </span><span>${authors[1]}</span>`;
   } else {
     return _join(
       _map(authors, (author, idx) =>
         idx < authors.length - 1
-          ? '<span>' + author + ', </span>'
+          ? `<span>${author}, </span>`
           : '<span class="and">and </span><span>' + author + '</span>'
       ),
       ''
