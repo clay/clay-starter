038c9f560548e77057d0a66f63b516956d7b5d70
james-owen <james.owen@nymag.com>
2019-04-16 11:07:16 -0400

    test files and site

---
+++ app/sites/doggo/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const publishing = require('../../services/server/publish-url'),
+  mainComponentRefs = ['article'];
+
+module.exports.routes = [
+  { path: '/' },
+  { path: '/dog' },
+  { path: '/:year/:month/:name' },
+  { path: '/article/:name' }
+];
+
+// Resolve the url to publish to
+module.exports.resolvePublishUrl = [
+  // Simple url format
+  (uri, data, locals) => publishing.getSlugUrl(data, locals, mainComponentRefs)
+];
