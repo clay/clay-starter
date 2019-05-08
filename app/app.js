26ab1f063e3a5ae52476f2011887606f4a01d59b
Pedro Rosario <pedrorosario@intellisys.com.do>
2019-05-08 10:29:23 -0400

    Add clustering

--- app/app.js
+++ app/app.js
@@ -2,19 +2,38 @@
 
 const pkg = require('./package.json'),
   express = require('express'),
+  cluster = require('cluster'),
   logger = require('./services/universal/log'),
   startup = require('./services/startup'),
   port = process.env.PORT || 3001,
   ip = process.env.IP_ADDRESS || '0.0.0.0';
 
 logger.init(pkg.version);
-let log = logger.setup({ file: __filename });
+let log = logger.setup({ file: __filename }),
+  app;
 
-startup(express())
-  .then(router => {
-    router.listen(port, ip);
-    log('info', `Clay listening on ${ip}:${port} (process ${process.pid})`);
-  })
-  .catch(error => {
-    log('error', error.message, { stack: error.stack });
+if (cluster.isMaster) {
+  const numCPUs = require('os').cpus().length;
+
+  log('info', `Creating ${numCPUs} worker(s)`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', worker => {
+    log('warn', `worker ${worker.process.pid} died`);
   });
+
+  log('info', 'Master process is running');
+} else {
+  app = express();
+  startup(app)
+    .then(router => {
+      router.listen(port, ip);
+      log('info', `Clay listening on ${ip}:${port} (process ${process.pid})`);
+    })
+    .catch(error => {
+      log('error', error.message, { stack: error.stack });
+    });
+}
